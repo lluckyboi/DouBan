0f2315b612501efb859ef6896bc706efe95d91a4
lluckyboi <53268752+lluckyboi@users.noreply.github.com>
2022-02-21 14:15:20 +0800

    Delete douban/dao directory

--- douban/dao/act.go
+++
@@ -1,9 +0,0 @@
-package dao
-
-import "douban/model"
-
-func InsertAct(act model.Act)error{
-	sqlstr:="insert into act(actor_id,role,film_id)values(?,?,?)"
-	_,err:=Db.Exec(sqlstr,act.ActorId,act.Role,act.FilmId)
-	return err
-}
